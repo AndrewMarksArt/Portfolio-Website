9b6d045fcfa96808ed2eb1b15f52cea5944ee988
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-11-10 09:51:54 -0800

    added about me sections with placeholders.

--- Andrew-Marks-Portfolio-Website/src/components/AboutMe.jsx
+++ Andrew-Marks-Portfolio-Website/src/components/AboutMe.jsx
@@ -2,7 +2,29 @@ export default function AboutMe() {
     return (
         <>
             <div className="section">
-                <h1>This is the About Me Section</h1>
+                <div className="container--about-me">
+                    <img className="logo--about-me" src="./img/logo.png"></img>
+                    <div className="about-me--main">
+                        <h1 className="about-me--title">About Me</h1>
+                        <h2 className="about-me--subtitle">Lorem ipsum dolor sit amet, 
+                        consectetur adipiscing elit.</h2>
+                        <p className="about-me--body">Lorem ipsum dolor sit amet, consectetur 
+                        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                        reprehenderit in voluptate velit.</p>
+                    </div>
+                    <div className="about-me--links">
+                        <h2 className="about-me--contact-title">CONTACT INFORMATION</h2>
+                        <ul className="about-me--socials-list">
+                            <li className="about-me--social-item">email</li>
+                            <li className="about-me--social-item">LinkedIn</li>
+                            <li className="about-me--social-item">Behance</li>
+                            <li className="about-me--social-item">Twitter</li>
+                            <li className="about-me--social-item">Github</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
             <hr className="seperator"></hr>
         </>

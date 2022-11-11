3b915cb0c7a00a28c89d5533ea68331a5351eda7
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-11-10 16:44:33 -0800

    updated nave bar so links are replaced by hamburger button on small screen.

--- Andrew-Marks-Portfolio-Website/src/components/NavBar.jsx
+++ Andrew-Marks-Portfolio-Website/src/components/NavBar.jsx
@@ -10,9 +10,13 @@ export default function NavBar() {
                     <li className="nav--list--item"><a href="#" className="nav--link">About</a></li>
                     <li className="nav--list--item"><a href="#" className="nav--link">Projects</a></li>
                     <li className="nav--list--item"><a href="#" className="nav--link">Resume</a></li>
-                    <li className="nav--list--item"><a href="#" className="nav--link--end">Artwork</a></li>
+                    <li className="nav--list--item"><a href="#" className="nav--link">Artwork</a></li>
                 </ul>
             </nav>
+
+            <button class="nav-toggle" aria-label="toggle navigation">
+                <span class="hamburger"></span>
+            </button>
             
         </header>
     );

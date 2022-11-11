fdefa14ea00941ca140b03d8e2ce03ad80d998bb
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-11-10 19:21:49 -0800

    created hamburger menu button to show/hide links for small screens/mobile

--- Andrew-Marks-Portfolio-Website/src/components/NavBar.jsx
+++ Andrew-Marks-Portfolio-Website/src/components/NavBar.jsx
@@ -1,4 +1,25 @@
+import React from "react";
+
 export default function NavBar() {
+
+    const [navOpen, setNavOpen] = React.useState(true)
+
+    function toggle() {
+        setNavOpen(!navOpen)
+        console.log(navOpen)
+        if (!navOpen) {
+            document.body.classList.remove('nav-open')
+        } else {
+            document.body.classList.toggle('nav-open')
+        }
+    }
+
+    function closeMenu() {
+        if(navOpen) {
+            document.body.classList.remove('nav-open')
+        }
+    }
+
     return (
         <header className="header">
             <div className="logo">
@@ -7,15 +28,15 @@ export default function NavBar() {
 
             <nav className="nav">
                 <ul className="nav--list">
-                    <li className="nav--list--item"><a href="#" className="nav--link">About</a></li>
-                    <li className="nav--list--item"><a href="#" className="nav--link">Projects</a></li>
-                    <li className="nav--list--item"><a href="#" className="nav--link">Resume</a></li>
-                    <li className="nav--list--item"><a href="#" className="nav--link">Artwork</a></li>
+                    <li className="nav--list--item"><a href="#about" className="nav--link" onClick={toggle}>About</a></li>
+                    <li className="nav--list--item"><a href="#" className="nav--link" onClick={toggle}>Projects</a></li>
+                    <li className="nav--list--item"><a href="#" className="nav--link" onClick={toggle}>Resume</a></li>
+                    <li className="nav--list--item"><a href="#" className="nav--link" onClick={toggle}>Artwork</a></li>
                 </ul>
             </nav>
 
-            <button class="nav-toggle" aria-label="toggle navigation">
-                <span class="hamburger"></span>
+            <button className="nav-toggle" aria-label="toggle navigation" onClick={toggle}>
+                <span className="hamburger"></span>
             </button>
             
         </header>

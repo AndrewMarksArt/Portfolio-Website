f7d73a1b9c50edc91f9b9b3a29d2e74afe4fb247
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-11-12 06:17:46 -0800

    added basic featured projects elements to home page, created project card template.

--- Andrew-Marks-Portfolio-Website/src/components/FeaturedProjects.jsx
+++ Andrew-Marks-Portfolio-Website/src/components/FeaturedProjects.jsx
@@ -1,8 +1,18 @@
+import ProjectCard from "./ProjectCard";
+
 export default function FeaturedProjects() {
     return (
         <>
-            <div className="section">
-                <h1>Here are my 3 top Projects</h1>
+            <div className="featured-container">
+                <div className="featured-top-line">
+                    <h1 className="featured-title">Featured Projects</h1>
+                    <button className="featured-see-all">see all projects</button>
+                </div>
+                <div className="featured-cards-container">
+                    <ProjectCard />
+                    <ProjectCard />
+                    <ProjectCard />
+                </div>
             </div>
             <hr className="seperator"></hr>
         </>
---
+++ Andrew-Marks-Portfolio-Website/src/components/ProjectCard.jsx
@@ -0,0 +1,9 @@
+export default function ProjectCard() {
+    return (
+        <div className="project-card">
+            <img></img>
+            <h2 className="project-title">PROJECT NAME</h2>
+            <p className="project-headline">This is a cool project</p>
+        </div>
+    )
+}

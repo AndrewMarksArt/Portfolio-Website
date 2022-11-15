feec160dcad564ab1d2815bc30a11ff65a6f9adf
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-11-15 08:25:16 -0800

    set styles and layout of featured project cards, adding template image for project cards

--- Andrew-Marks-Portfolio-Website/src/components/ProjectCard.jsx
+++ Andrew-Marks-Portfolio-Website/src/components/ProjectCard.jsx
@@ -1,7 +1,7 @@
 export default function ProjectCard() {
     return (
         <div className="project-card">
-            <img></img>
+            <img src="./img/shogun_featured.png" className="project-image"></img>
             <h2 className="project-title">PROJECT NAME</h2>
             <p className="project-headline">This is a cool project</p>
         </div>

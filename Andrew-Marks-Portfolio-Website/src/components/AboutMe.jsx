b14133190e665a9cef05bfa479789eb44bc53449
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-11-18 16:23:01 -0800

    removed styling from about me, need to redo and reorganzie, added main section container to seperate logo, headline, body text from contact information.

--- Andrew-Marks-Portfolio-Website/src/components/AboutMe.jsx
+++ Andrew-Marks-Portfolio-Website/src/components/AboutMe.jsx
@@ -4,23 +4,26 @@ export default function AboutMe() {
     return (
         <>
             <div className="container--about-me">
-                <div className="about-me--logo-container">
-                    <img className="logo--about-me" src="./img/logo.png"></img>
-                </div>
-                <div className="about-me--main">
-                    <h1 className="about-me--title">About Me</h1>
+                <div className="about-me--main-container">
+                    <div className="about-me--logo-container">
+                        <img className="logo--about-me" src="./img/logo.png"></img>
+                    </div>
+                    <div className="about-me--main">
+                        <h1 className="about-me--title">About Me</h1>
 
-                    <h2 className="about-me--subtitle">Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit.</h2>
+                        <h2 className="about-me--subtitle">Lorem ipsum dolor sit amet, 
+                        consectetur adipiscing elit.</h2>
 
-                    <p className="about-me--body">Lorem ipsum dolor sit amet, consectetur 
-                    adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-                    magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                    reprehenderit in voluptate velit.</p>
+                        <p className="about-me--body">Lorem ipsum dolor sit amet, consectetur 
+                        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                        reprehenderit in voluptate velit.</p>
+                    </div>
                 </div>
-                
-                <ContactMe />
+                <div className="about-me--contact-container">
+                    <ContactMe />
+                </div>
                     
             </div>
             <hr className="seperator"></hr>

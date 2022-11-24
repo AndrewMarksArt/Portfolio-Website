56694f3cf6f686d456ecb444346542a1e600a621
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-11-24 06:12:51 -0800

    added modal styles to image, container, title, and button for small screens

--- Andrew-Marks-Portfolio-Website/src/components/Gallery/ImageModal.jsx
+++ Andrew-Marks-Portfolio-Website/src/components/Gallery/ImageModal.jsx
@@ -6,6 +6,7 @@ export default function Modal(props) {
         <div className="modal" onClick={props.onClose}>
             <div className="modal-content" onClick={e => e.stopPropagation()}>
                 <img src={`./img/gallery/${props.img}`} className="modal-image"></img>
+                <h1 className="modal-image-title">{props.img}</h1>
                 <button className="modal-close" onClick={props.onClose}>close</button>
             </div>
         </div>

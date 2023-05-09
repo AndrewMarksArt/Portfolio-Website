f49b06ed36eb32ffa32c8af312262dcbe2cd7903
AndrewMarksArt <andrew.colin.marks@gmail.com>
2023-05-09 14:49:14 -0500

    fixed project card sizing and spacing issue

--- Andrew-Marks-Portfolio-Website/src/components/DataScienceProjects.jsx
+++ Andrew-Marks-Portfolio-Website/src/components/DataScienceProjects.jsx
@@ -14,9 +14,16 @@ export default function DsProjects() {
     }) 
 
     return (
-        <div className="projects--ds-container">
+        <div className="projects--uiux-container">
+            <div className="projects--top-bar">
+                <div className="projects--line"></div>
+                <div className="projects--line"></div>
+                <div className="projects--line"></div>
+            </div>
+
             <div className="">
                 <h1 className="projects--section-title">Data Science Projects</h1>
+                <h2 className="projects--section-subhead">Focus: Skill 1, Skill 2, Skill 3, Skill 4, Skill 5</h2>
             </div>
 
             <div className="projects--cards-container">

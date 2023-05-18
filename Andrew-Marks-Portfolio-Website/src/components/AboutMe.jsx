5d7b72ac2b588303321dc81cac5280166ded7f4f
AndrewMarksArt <andrew.colin.marks@gmail.com>
2023-05-18 14:27:11 -0500

    updated About me section with name, sub-head, and blurb

--- Andrew-Marks-Portfolio-Website/src/components/AboutMe.jsx
+++ Andrew-Marks-Portfolio-Website/src/components/AboutMe.jsx
@@ -16,16 +16,17 @@ export default function AboutMe() {
                         <img className="logo--about-me" src="../img/logo.png"></img>
                     </div>
                     <div className="about-me--main">
-                        <h1 className="about-me--title">About Me</h1>
+                        <h1 className="about-me--title">Hi, my name is Andrew </h1>
 
-                        <h2 className="about-me--subtitle">Lorem ipsum dolor sit amet, 
-                        consectetur adipiscing elit.</h2>
+                        <h2 className="about-me--subtitle">I love building fun and unique experiences that users love!</h2>
 
-                        <p className="about-me--body">Lorem ipsum dolor sit amet, consectetur 
-                        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                        reprehenderit in voluptate velit.</p>
+                        <p className="about-me--body"> As someone who has faced personal challenges with chronic pain, I understand the 
+                        importance of creating accessible and enjoyable products. I believe that by prioritizing accessibility and fun, 
+                        businesses can achieve remarkable results and create meaningful impact.</p>
+                        
+                        <p className="about-me--body"> With a strong background in UI/UX design 
+                        and a passion for creating delightful user experiences, I strive to bridge the gap between user needs and 
+                        business goals.</p>
                     </div>
                 </div>
                 <div className="about-me--contact-container">

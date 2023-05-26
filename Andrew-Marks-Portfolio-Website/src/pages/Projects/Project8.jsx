46d0d133d2d098f8f4c1ffa974cc4ef6180b6284
AndrewMarksArt <andrew.colin.marks@gmail.com>
2023-05-26 11:31:00 -0500

    added styles for project 8 yelp nlp

--- Andrew-Marks-Portfolio-Website/src/pages/Projects/Project8.jsx
+++ Andrew-Marks-Portfolio-Website/src/pages/Projects/Project8.jsx
@@ -43,10 +43,10 @@ export default function Project8() {
                     <h2 className="project8--into-title">
                         Project Goals:
                     </h2>
-                    <ul className="project8--list">
+                    <ol className="project8--list">
                         <li className="project8--list-item">
-                            Predict whether a review is ‘positive’ or ‘negative’ based on the words used in the review (positive is any review with 
-                            4 or 5 stars, and negative reviews are those with 3 stars or less).
+                            <b>Predict whether a review is ‘positive’ or ‘negative’ based on the words used in the review (positive is any review with 
+                            4 or 5 stars, and negative reviews are those with 3 stars or less).</b>
                         </li>
                         <li className="project8--list-item">
                             Create a sentiment score for each business based on the words used in each of reviews about the business.  — (future analysis)
@@ -54,11 +54,11 @@ export default function Project8() {
                         <li className="project8--list-item">
                             Predict whether a business will succeed or fail. — (future analysis)
                         </li>
-                    </ul>
+                    </ol>
                 </div>
 
                 <div className="project8--getting-started-container">
-                    <h2 className="project8--started-title">
+                    <h2 className="project8--intro-title">
                         Getting Started, What is Sentiment Analysis?
                     </h2>
 
@@ -67,9 +67,9 @@ export default function Project8() {
                     </p>
 
                     <p className="project8--quote">
-                        Sentiment analysis uses data mining processes and techniques to extract and capture data for analysis in order to discern the 
+                        <i>"Sentiment analysis uses data mining processes and techniques to extract and capture data for analysis in order to discern the 
                         subjective opinion of a document or collection of documents, like blog posts, reviews, news articles and social media feeds like 
-                        tweets and status updates.
+                        tweets and status updates."</i>
                     </p>
 
                     <p className="project8--body-text">
@@ -86,11 +86,11 @@ export default function Project8() {
                             Negative reviews = 3 stars or lower
                         </li>
                         <li className="project8--list-item">
-                            Of the almost 6,700,000 reviews about 64% are positive and 36% are negative.
+                            Of the almost 6,700,000 reviews about <b>64% are positive and 36% are negative.</b>
                         </li>
                     </ul>
 
-                    <h2 className="project8--started-title">
+                    <h2 className="project8--intro-title">
                         Imports
                     </h2>
 
@@ -115,20 +115,20 @@ export default function Project8() {
 
                 <div className="project8--preparing-container">
 
-                    <h2 className="project8--preparing-title">
+                    <h2 className="project8--intro-title">
                         Preparing the Data
                     </h2>
 
-                    <p className="project8--preparing-text">
+                    <p className="project8--body-text">
                         For the Bag of Words (BOW) approach we could use all unique words however it will be very expensive computationally and there are lots or 
                         words such as ‘a’ or ‘the’ which care very little importance so we will need to do some filtering.  
                     </p>
 
-                    <p className="project8--preparing-text">
-                        Filtering steps for each review:  
+                    <p className="project8--body-text">
+                        <b>Filtering steps for each review:</b>  
                     </p>
 
-                    <ul className="project8--list">
+                    <ol className="project8--list">
                         <li className="project8--list-item">
                             Remove punctuation
                         </li>
@@ -147,47 +147,49 @@ export default function Project8() {
                         <li className="project8--list-item">
                             Create a list of tuples with the review and label
                         </li>
-                    </ul>
+                    </ol>
 
                     <img className="project8--process-img" src="../img/case_studies/project8/bow_process.png"/>
 
                 </div>
 
                 <div className="project8--results-container">
-                    <h2 className="project8--results-title">
+                    <h2 className="project8--intro-title">
                         Create Features
                     </h2>
-                    <p className="project8--results-text">
+                    <p className="project8--body-text">
                         We will take the list of all words from above and create a dictionary where the keys are the words and the values are the counts of 
                         each word. From there we can take 3500 the most frequent words and create a feature set list by checking each review against the 3500 
                         most frequent words. From there I created a training and testing set with the training set using 40,000 reviews to train and 10,000 to test.  
                     </p>
                     <img className="project8--top-words-img" src="../img/case_studies/project8/top_words.png"/>
-                    <h2 className="project8--results-title">
+                    <h2 className="project8--intro-title">
                         Baseline Model vs Naive Bayes Classifier
                     </h2>
-                    <p className="project8--results-text">
+                    <p className="project8--body-text">
                         For the baseline model we simply use the majority class of ‘positive’ for all reviews giving us an accuracy of 65.84%  
                     </p>
-                    <p className="project8--results-text">
+                    <p className="project8--body-text">
                         The first model I compared the baseline to was a Naive Bayes Classifier which has an accuracy of 77.19% which is a significant 
                         improvement over the baseline. 
                     </p>
-                    <p className="project8--results-text">
+                    <p className="project8--body-text">
                         To dig deeper into the results we can look the top 10 most informative words for negative reviews and positive reviews, and these 
                         results seem to make sense.
                     </p>
-                    <img className="project8--top-neg-img" src="../img/case_studies/project8/top_neg.png"/>
-                    <img className="project8--top-pos-img" src="../img/case_studies/project8/top_pos.png"/>
-                    <p className="project8--results-text">
+                    <div className="project8--side-by-side">
+                        <img className="project8--top-neg-img" src="../img/case_studies/project8/top_neg.png"/>
+                        <img className="project8--top-pos-img" src="../img/case_studies/project8/top_pos.png"/>
+                    </div>
+                    <p className="project8--body-text">
                         However something interesting is how mush more important negative words are to classifying a review than positive words are, when we look 
                         at the top 25 most important words only 4 are positive and the rest are negative and only 1 positive is in the top 10 most important.
                     </p>
                     <img className="project8--important-img" src="../img/case_studies/project8/most_important.png"/>
-                    <p className="project8--results-text">
+                    <p className="project8--body-text">
                         Since the Naive Bayes out preformed the base line model, I tried a few others:
                     </p>
-                    <ul className="project8--list">
+                    <ol className="project8--list">
                         <li className="project8--list-item">
                             Multinomial Naive Bayes Classifier accuracy: 84.3%
                         </li>
@@ -203,14 +205,14 @@ export default function Project8() {
                         <li className="project8--list-item">
                             Linear Support Vector Classifier accuracy: 85.6%
                         </li>
-                    </ul>
+                    </ol>
                 </div>
 
                 <div className="project8--final-container">
-                    <h2 className="project8--final-title">
+                    <h2 className="project8--intro-title">
                         Final Thoughts
                     </h2>
-                    <p className="project8--final-text">
+                    <p className="project8--body-text">
                         It seems that by taking even a simple Bag of Words approach where lots of context is lost we can still fairly accurately classify reviews 
                         as positive or negative. I hope to be able to create an ensemble model next that should also give me a confidence score for each prediction 
                         which I’m hoping to be able to use to create new feature along with the most important features. Once I have my ensemble model I can run all 
@@ -218,10 +220,10 @@ export default function Project8() {
                         not a business will fail based on the reviews is it getting and if it is in danger of failing the business owner can have more information 
                         into what is driving the worst reviews and make changes accordingly. 
                     </p>
-                    <h2 className="project8--final-title">
+                    <h2 className="project8--intro-title">
                         Next Steps:
                     </h2>
-                    <ul className="project8--list">
+                    <ol className="project8--list">
                         <li className="project8--list-item">
                             Building an Ensemble Model
                         </li>
@@ -231,8 +233,8 @@ export default function Project8() {
                         <li className="project8--list-item">
                             Use features with the businesses.json file to predict if a business will succeed or fail. 
                         </li>
-                    </ul>
-                    <p className="project8--final-text">
+                    </ol>
+                    <p className="project8--body-text">
                         <a 
                             href="https://github.com/AndrewMarksArt/yelp_data_applied_modeling" 
                             target="_blank"

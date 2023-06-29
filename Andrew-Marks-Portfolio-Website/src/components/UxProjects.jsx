d47aba5e68f5732876c8e6fffac1428b8b6acb39
AndrewMarksArt <andrew.colin.marks@gmail.com>
2023-06-29 11:27:39 -0500

    split Shogun War project into 2 seperate projects. UI/UX audit and fixes as part 1 and level design as part 2.

--- Andrew-Marks-Portfolio-Website/src/components/UxProjects.jsx
+++ Andrew-Marks-Portfolio-Website/src/components/UxProjects.jsx
@@ -33,7 +33,7 @@ export default function UIUXProjects() {
                 <Link to="/project-3" style={{ textDecoration: 'none' }}>{projectElements[2]}</Link>
                 <Link to="/project-4" style={{ textDecoration: 'none' }}>{projectElements[3]}</Link>
                 <Link to="/project-5" style={{ textDecoration: 'none' }}>{projectElements[4]}</Link>
-                <Link to="/project-1" style={{ textDecoration: 'none' }}>{projectElements[5]}</Link>
+                <Link to="/project-6" style={{ textDecoration: 'none' }}>{projectElements[5]}</Link>
             </div>
 
         </div>

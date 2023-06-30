499b63a19d85b6060e9b4f13063f068e26cf8459
AndrewMarksArt <andrew.colin.marks@gmail.com>
2023-06-30 16:59:47 -0500

    changed about me container boarder to a box shadow

--- Andrew-Marks-Portfolio-Website/src/pages/Projects/BattleWarriors1.jsx
+++ Andrew-Marks-Portfolio-Website/src/pages/Projects/BattleWarriors1.jsx
@@ -142,7 +142,7 @@ export default function Project3() {
 
 
                 <div className="project3--video-container">
-                    <video src="../videos/example_viking.mp4" playsinline loop muted controls className="char-build-video"/>
+                    <video src="../videos/example_viking.mp4" className="char-build-video" autoplay playsInline loop muted controls webkit-playsInline/>
                 </div>
             </div>
 

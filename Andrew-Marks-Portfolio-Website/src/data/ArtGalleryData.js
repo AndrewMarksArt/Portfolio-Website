2f0b31d2ccb493f9eabe96efa0ec8ca51cd7c346
AndrewMarksArt <andrew.colin.marks@gmail.com>
2023-07-02 15:41:46 -0500

    replaced all large and thumbnail pngs with WebP images for the gallery

--- Andrew-Marks-Portfolio-Website/src/data/ArtGalleryData.js
+++ Andrew-Marks-Portfolio-Website/src/data/ArtGalleryData.js
@@ -1,184 +1,184 @@
 export default [
     {
         id: 1,
-        img: "thumbnail/GoT.png",
-        full_img: "large/GoT_LRG.png",
+        img: "thumbnail/GoT copy.webp",
+        full_img: "large/GoT_LRG copy.webp",
         title: "Tyrion Lannister",
         desc: "pen & ink, illustrator",
         type: "drawing"
     },
     {
         id: 2,
-        img: "thumbnail/tangled.png",
-        full_img: "large/tangled_LRG.png",
+        img: "thumbnail/tangled copy.webp",
+        full_img: "large/tangled_LRG copy.webp",
         title: "Tangled",
         desc: "pen & ink, illustrator",
         type: "drawing"
     },
     {
         id: 3,
-        img: "thumbnail/gn.png",
-        full_img: "large/gn_LRG.png",
+        img: "thumbnail/gn copy.webp",
+        full_img: "large/gn_LRG copy.webp",
         title: "Game Over",
         desc: "battle warriors icon",
         type: "game_art"
     },
     {
         id: 4,
-        img: "thumbnail/greater_chest.png",
-        full_img: "large/greater_chest_LRG.png",
+        img: "thumbnail/greater_chest copy.webp",
+        full_img: "large/greater_chest_LRG copy.webp",
         title: "Greater Chest",
         desc: "battle warriors game reward",
         type: "game_art"
     },
     {
         id: 5,
-        img: "thumbnail/rain.png",
-        full_img: "large/rain_LRG.png",
+        img: "thumbnail/rain copy.webp",
+        full_img: "large/rain_LRG copy.webp",
         title: "Rainy Day",
         desc: "watercolor, ink, canvas",
         type: "painting"
     },
     {
         id: 6,
-        img: "thumbnail/hipster_cat.png",
-        full_img: "large/hipster_cat_LRG.png",
+        img: "thumbnail/hipster_cat copy.webp",
+        full_img: "large/hipster_cat_LRG copy.webp",
         title: "Hipster Cat",
         desc: "pen & ink, illustrator",
         type: "drawing"
     },
     {
         id: 7,
-        img: "grace_kelly.png",
-        full_img: "large/grace_kelly_LRG.png",
+        img: "thumbnail/grace_kelly copy.webp",
+        full_img: "large/grace_kelly_LRG copy.webp",
         title: "Grace Kelly",
         desc: "acrylic, ink, canvas",
         type: "painting"
     },
     {
         id: 8,
-        img: "thumbnail/small_chest.png",
-        full_img: "large/small_chest_LRG.png",
+        img: "thumbnail/small_chest copy.webp",
+        full_img: "large/small_chest_LRG copy.webp",
         title: "Small Chest",
         desc: "battle warriors game reward",
         type: "game_art"
     },
     {
         id: 9,
-        img: "thumbnail/armory.png",
-        full_img: "large/armory_LRG.png",
+        img: "thumbnail/armory copy.webp",
+        full_img: "large/armory_LRG copy.webp",
         title: "Pick Your Weapon",
         desc: "battle warriors basic weapons",
         type: "game_art"
     },
     {
         id: 10,
-        img: "thumbnail/balloons.png",
-        full_img: "large/balloons_LRG.png",
+        img: "thumbnail/balloons copy.webp",
+        full_img: "large/balloons_LRG copy.webp",
         title: "Float Away",
         desc: "pen & ink, illustrator",
         type: "drawing"
     },
     {
         id: 11,
-        img: "thumbnail/headdress.png",
-        full_img: "large/headdress_LRG.png",
+        img: "thumbnail/headdress copy.webp",
+        full_img: "large/headdress_LRG copy.webp",
         title: "Native Beauty",
         desc: "pen & ink, illustrator",
         type: "drawing"
     },
     {
         id: 12,
-        img: "thumbnail/messy_bun.png",
-        full_img: "large/messy_bun_LRG.png",
+        img: "thumbnail/messy_bun copy.webp",
+        full_img: "large/messy_bun_LRG copy.webp",
         title: "Messy Bun",
         desc: "pen & ink, illustrator",
         type: "drawing"
     },
     {
         id: 13,
-        img: "thumbnail/lightbulb.png",
-        full_img: "large/lightbulb_LRG.png",
+        img: "thumbnail/lightbulb copy.webp",
+        full_img: "large/lightbulb_LRG copy.webp",
         title: "Upcycled",
         desc: "pen & ink, illustrator",
         type: "drawing"
     },
     {
         id: 14,
-        img: "thumbnail/my_fair_lady.png",
-        full_img: "large/my_fair_lady_LRG.png",
+        img: "thumbnail/my_fair_lady copy.webp",
+        full_img: "large/my_fair_lady_LRG copy.webp",
         title: "My Fair Lady",
         desc: "pen & ink, illustrator",
         type: "drawing"
     },
     {
         id: 15,
-        img: "thumbnail/little_princess.png",
-        full_img: "large/little_princess_LRG.png",
+        img: "thumbnail/little_princess copy.webp",
+        full_img: "large/little_princess_LRG copy.webp",
         title: "Little Princess",
         desc: "pen & ink, illustrator",
         type: "drawing"
     },
     {
         id: 16,
-        img: "thumbnail/upside_down.png",
-        full_img: "large/upside_down_LRG.png",
+        img: "thumbnail/upside_down copy.webp",
+        full_img: "large/upside_down_LRG copy.webp",
         title: "New Perspective",
         desc: "pen & ink, illustrator",
         type: "drawing"
     },
     {
         id: 17,
-        img: "thumbnail/batman.png",
-        full_img: "large/batman_LRG.png",
+        img: "thumbnail/batman copy.webp",
+        full_img: "large/batman_LRG copy.webp",
         title: "Inking Practice",
         desc: "pen & ink, illustrator",
         type: "drawing"
     },
     {
         id: 18,
-        img: "thumbnail/lion.png",
-        full_img: "large/lion_LRG.png",
+        img: "thumbnail/lion copy.webp",
+        full_img: "large/lion_LRG copy.webp",
         title: "Squeaky",
         desc: "character design",
         type: "game_art"
     },
     {
         id: 19,
-        img: "thumbnail/lege_sword.png",
-        full_img: "large/lege_sword_LRG.png",
+        img: "thumbnail/lege_sword copy.webp",
+        full_img: "large/lege_sword_LRG copy.webp",
         title: "Oversword",
         desc: "battle warriors legendary item",
         type: "game_art"
     },
     {
         id: 20,
-        img: "thumbnail/flames.png",
-        full_img: "large/flames_LRG.png",
+        img: "thumbnail/flames copy.webp",
+        full_img: "large/flames_LRG copy.webp",
         title: "Into the Flames",
         desc: "Acrylic, canvas",
         type: "painting"
     },
     {
         id: 21,
-        img: "thumbnail/witch.png",
-        full_img: "large/witch_LRG.png",
+        img: "thumbnail/witch copy.webp",
+        full_img: "large/witch_LRG copy.webp",
         title: "Sabrina",
         desc: "character design",
         type: "game_art"
     },
     {
         id: 22,
-        img: "thumbnail/vampire.png",
-        full_img: "large/vampire_LRG.png",
+        img: "thumbnail/vampire copy.webp",
+        full_img: "large/vampire_LRG copy.webp",
         title: "Chompy",
         desc: "character design",
         type: "game_art"
     },
     {
         id: 23,
-        img: "thumbnail/drips.png",
-        full_img: "large/drips_LRG.png",
+        img: "thumbnail/drips copy.webp",
+        full_img: "large/drips_LRG copy.webp",
         title: "Squeeze",
         desc: "spray pain, ink, canvas",
         type: "painting"
@@ -186,8 +186,8 @@ export default [
     
     {
         id: 24,
-        img: "thumbnail/crazy_love.png",
-        full_img: "large/crazy_love_LRG.png",
+        img: "thumbnail/crazy_love copy.webp",
+        full_img: "large/crazy_love_LRG copy.webp",
         title: "Crazy Love",
         desc: "Acrylic, canvas",
         type: "painting"
@@ -195,8 +195,8 @@ export default [
 
     {
         id: 25,
-        img: "thumbnail/VintageDress.png",
-        full_img: "large/VintageDress_LRG.png",
+        img: "thumbnail/VintageDress copy.webp",
+        full_img: "large/VintageDress_LRG copy.webp",
         title: "Vintage Dress",
         desc: "Acrylic, ink, canvas",
         type: "painting"
@@ -204,184 +204,184 @@ export default [
 
     {
         id: 26,
-        img: "thumbnail/elephant.png",
-        full_img: "large/elephant_LRG.png",
+        img: "thumbnail/elephant copy.webp",
+        full_img: "large/elephant_LRG copy.webp",
         title: "Elle Trunks",
         desc: "character design",
         type: "game_art"
     },
     {
         id: 27,
-        img: "thumbnail/wl_items.png",
-        full_img: "large/wl_items_LRG.png",
+        img: "thumbnail/wl_items copy.webp",
+        full_img: "large/wl_items_LRG copy.webp",
         title: "Light Bringer Set",
         desc: "battle warriors whitelist rewards",
         type: "game_art"
     },
     {
         id: 28,
-        img: "thumbnail/bw_banner.png",
-        full_img: "large/bw_banner_LRG.png",
+        img: "thumbnail/bw_banner copy.webp",
+        full_img: "large/bw_banner_LRG copy.webp",
         title: "All Stars",
         desc: "banner with example characters",
         type: "game_art"
     },
     {
         id: 29,
-        img: "thumbnail/berserker.png",
-        full_img: "large/berserker_LRG.png",
+        img: "thumbnail/berserker copy.webp",
+        full_img: "large/berserker_LRG copy.webp",
         title: "Berserker",
         desc: "battle warriors character design",
         type: "game_art"
     },
     {
         id: 30,
-        img: "thumbnail/death_mountains.png",
-        full_img: "large/death_mountains_LRG.png",
+        img: "thumbnail/death_mountains copy.webp",
+        full_img: "large/death_mountains_LRG copy.webp",
         title: "Thane",
         desc: "battle warriors character design",
         type: "game_art"
     },
     {
         id: 31,
-        img: "thumbnail/magic_high_king.png",
-        full_img: "large/magic_high_king_LRG.png",
+        img: "thumbnail/magic_high_king copy.webp",
+        full_img: "large/magic_high_king_LRG copy.webp",
         title: "Magic High King",
         desc: "battle warriors character design",
         type: "game_art"
     },
     {
         id: 32,
-        img: "thumbnail/freya_valk.png",
-        full_img: "large/freya_valk_LRG.png",
+        img: "thumbnail/freya_valk copy.webp",
+        full_img: "large/freya_valk_LRG copy.webp",
         title: "Valkyrie Freya",
         desc: "battle warriors character design",
         type: "game_art"
     },
     {
         id: 33,
-        img: "thumbnail/rouge.png",
-        full_img: "large/rouge_LRG.png",
+        img: "thumbnail/rouge copy.webp",
+        full_img: "large/rouge_LRG copy.webp",
         title: "Dizzy Rouge",
         desc: "battle warriors character design",
         type: "game_art"
     },
     {
         id: 34,
-        img: "thumbnail/heal_valk.png",
-        full_img: "large/heal_valk_LRG.png",
+        img: "thumbnail/heal_valk copy.webp",
+        full_img: "large/heal_valk_LRG copy.webp",
         title: "Valkyrie Healer",
         desc: "battle warriors character design",
         type: "game_art"
     },
     {
         id: 35,
-        img: "thumbnail/thor.png",
-        full_img: "large/thor_LRG.png",
+        img: "thumbnail/thor copy.webp",
+        full_img: "large/thor_LRG copy.webp",
         title: "Ragnar Redmane",
         desc: "battle warriors character design",
         type: "game_art"
     },
     {
         id: 36,
-        img: "thumbnail/trainee.png",
-        full_img: "large/trainee_LRG.png",
+        img: "thumbnail/trainee copy.webp",
+        full_img: "large/trainee_LRG copy.webp",
         title: "Viking Trainee",
         desc: "battle warriors character design",
         type: "game_art"
     },
     {
         id: 37,
-        img: "thumbnail/ranger.png",
-        full_img: "large/ranger_LRG.png",
+        img: "thumbnail/ranger copy.webp",
+        full_img: "large/ranger_LRG copy.webp",
         title: "Forest Ranger",
         desc: "battle warriors character design",
         type: "game_art"
     },
     {
         id: 38,
-        img: "thumbnail/queen_freya.png",
-        full_img: "large/queen_freya_LRG.png",
+        img: "thumbnail/queen_freya copy.webp",
+        full_img: "large/queen_freya_LRG copy.webp",
         title: "High Queen Freya",
         desc: "battle warriors character design",
         type: "game_art"
     },
     {
         id: 39,
-        img: "thumbnail/death_berserker.png",
-        full_img: "large/death_berserker_LRG.png",
+        img: "thumbnail/death_berserker copy.webp",
+        full_img: "large/death_berserker_LRG copy.webp",
         title: "Death Berserker",
         desc: "battle warriors character design",
         type: "game_art"
     },
     {
         id: 40,
-        img: "thumbnail/bronson.png",
-        full_img: "large/bronson_LRG.png",
+        img: "thumbnail/bronson copy.webp",
+        full_img: "large/bronson_LRG copy.webp",
         title: "Bronson",
         desc: "battle warriors character design",
         type: "game_art"
     },
     {
         id: 41,
-        img: "thumbnail/freya_concept.png",
-        full_img: "large/freya_concept_LRG.png",
+        img: "thumbnail/freya_concept copy.webp",
+        full_img: "large/freya_concept_LRG copy.webp",
         title: "Powers Concept",
         desc: "battle warriors character design",
         type: "game_art"
     },
     {
         id: 42,
-        img: "thumbnail/ragnar_assassin.png",
-        full_img: "large/ragnar_assassin_LRG.png",
+        img: "thumbnail/ragnar_assassin copy.webp",
+        full_img: "large/ragnar_assassin_LRG copy.webp",
         title: "Assassin Ragnar",
         desc: "battle warriors character design",
         type: "game_art"
     },
     {
         id: 43,
-        img: "thumbnail/rage_queen.png",
-        full_img: "large/rage_queen_LRG.png",
+        img: "thumbnail/rage_queen copy.webp",
+        full_img: "large/rage_queen_LRG copy.webp",
         title: "Rage High Queen",
         desc: "battle warriors character design",
         type: "game_art"
     },
     {
         id: 44,
-        img: "thumbnail/death_king.png",
-        full_img: "large/death_king_LRG.png",
+        img: "thumbnail/death_king copy.webp",
+        full_img: "large/death_king_LRG copy.webp",
         title: "Death High King",
         desc: "battle warriors character design",
         type: "game_art"
     },
     {
         id: 45,
-        img: "thumbnail/poision_queen.png",
-        full_img: "large/poision_queen_LRG.png",
+        img: "thumbnail/poision_queen copy.webp",
+        full_img: "large/poision_queen_LRG copy.webp",
         title: "Poison Queen",
         desc: "battle warriors character design",
         type: "game_art"
     },
     {
         id: 46,
-        img: "thumbnail/poision_king.png",
-        full_img: "large/poision_king_LRG.png",
+        img: "thumbnail/poision_king copy.webp",
+        full_img: "large/poision_king_LRG copy.webp",
         title: "Poison King",
         desc: "battle warriors character design",
         type: "game_art"
     },
     {
         id: 47,
-        img: "thumbnail/blood_queen.png",
-        full_img: "large/blood_queen_LRG.png",
+        img: "thumbnail/blood_queen copy.webp",
+        full_img: "large/blood_queen_LRG copy.webp",
         title: "Blood Queen",
         desc: "battle warriors character design",
         type: "game_art"
     },
     {
         id: 48,
-        img: "thumbnail/magic_archer.png",
-        full_img: "large/magic_archer_LRG.png",
+        img: "thumbnail/magic_archer copy.webp",
+        full_img: "large/magic_archer_LRG copy.webp",
         title: "Magic Archer",
         desc: "battle warriors character design",
         type: "game_art"
@@ -390,95 +390,95 @@ export default [
 
     {
         id: 49,
-        img: "thumbnail/hard.png",
-        full_img: "large/hard_LRG.png",
+        img: "thumbnail/hard copy.webp",
+        full_img: "large/hard_LRG copy.webp",
         title: "Watch Your Step",
         desc: "battle warriors game design",
         type: "ui"
     },
     {
         id: 50,
-        img: "thumbnail/level_building.png",
-        full_img: "large/level_building_LRG.png",
+        img: "thumbnail/level_building copy.webp",
+        full_img: "large/level_building_LRG copy.webp",
         title: "Jump or Duck?",
         desc: "battle warriors game design",
         type: "ui"
     },
     {
         id: 51,
-        img: "thumbnail/good_luck.png",
-        full_img: "large/good_luck_LRG.png",
+        img: "thumbnail/good_luck copy.webp",
+        full_img: "large/good_luck_LRG copy.webp",
         title: "Good Luck...",
         desc: "battle warriors game design",
         type: "ui"
     },
     {
         id: 52,
-        img: "thumbnail/char_select.png",
-        full_img: "large/char_select_LRG.png",
+        img: "thumbnail/char_select copy.webp",
+        full_img: "large/char_select_LRG copy.webp",
         title: "Character Select",
         desc: "battle warriors game design",
         type: "ui"
     },
     {
         id: 53,
-        img: "thumbnail/new_game.png",
-        full_img: "large/new_game_LRG.png",
+        img: "thumbnail/new_game copy.webp",
+        full_img: "large/new_game_LRG copy.webp",
         title: "New Game",
         desc: "battle warriors game design",
         type: "ui"
     },
     {
         id: 54,
-        img: "thumbnail/ui_window.png",
-        full_img: "large/ui_window_LRG.png",
+        img: "thumbnail/ui_window copy.webp",
+        full_img: "large/ui_window_LRG copy.webp",
         title: "UI Elements",
         desc: "battle warriors game ui design",
         type: "ui"
     },
     {
         id: 55,
-        img: "thumbnail/weather_app.png",
-        full_img: "large/weather_app_LRG.png",
+        img: "thumbnail/weather_app copy.webp",
+        full_img: "large/weather_app_LRG copy.webp",
         title: "Weather App",
         desc: "ui design practice",
         type: "ui"
     },
     {
         id: 56,
-        img: "thumbnail/meditation.png",
-        full_img: "large/meditation_LRG.png",
+        img: "thumbnail/meditation copy.webp",
+        full_img: "large/meditation_LRG copy.webp",
         title: "Meditation App",
         desc: "ui design practice",
         type: "ui"
     },
     {
         id: 57,
-        img: "thumbnail/boarding_pass.png",
-        full_img: "large/boarding_pass_LRG.png",
+        img: "thumbnail/boarding_pass copy.webp",
+        full_img: "large/boarding_pass_LRG copy.webp",
         title: "Boading Pass",
         desc: "ui design practice",
         type: "ui"
     },
     {
         id: 58,
-        img: "thumbnail/dashboard_mock.png",
-        full_img: "large/dashboard_mock.png",
+        img: "thumbnail/dashboard_mock copy.webp",
+        full_img: "large/dashboard_mock copy.webp",
         title: "Dashboard Concept",
         desc: "ui design practice",
         type: "ui"
     },
     {
         id: 59,
-        img: "thumbnail/azuki_login.png",
-        full_img: "large/azuki_login_LRG.png",
+        img: "thumbnail/azuki_login copy.webp",
+        full_img: "large/azuki_login_LRG copy.webp",
         title: "NFT Login Idea",
         desc: "ui design practice",
         type: "ui"
     },
     {
         id: 60,
-        img: "thumbnail/crypto_dashboard.png",
+        img: "thumbnail/crypto_dashboard copy.webp",
         full_img: "large/crypto_dashboard_LRG.jpg",
         title: "Discord Security Tool",
         desc: "ui design practice",
